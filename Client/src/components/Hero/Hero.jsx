4c2dacc7cbbff4e490502de5ecbd2ab7cdacfb52
user.email "dhruvknipsss023@gmail.com <dhruvknipsss023@gmail.com>
2025-03-02 23:43:55 +0530

    some changes added

--- Client/src/components/Hero/Hero.jsx
+++ Client/src/components/Hero/Hero.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import BgImage from "../../assets/bg-slate.png";
+
+const bgImage = {
+  backgroundImage : `url(${BgImage})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+};
 
 const Hero = () => {
   return (
-    <main>
+    <main style={bgImage}>
         <section>
             <div className="container">
               {/* Navbar Section */}
               <div className='grid grid-cols-1 md:grid-cols-2'>
-
               </div>
             </div>
         </section>
