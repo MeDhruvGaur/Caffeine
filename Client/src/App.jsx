cb968e8b31622e74757098b83ff696f165259315
user.email "dhruvknipsss023@gmail.com <dhruvknipsss023@gmail.com>
2025-04-21 21:48:25 +0530

    added changes in Navbar and assests

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Services from './components/Services/Services';
 
 const App = () => {
   return (
     <div className='overflow-x-hidden'>
+      <Navbar />
       <Hero/>
       <Services />
     </div>
